0540552cf27d1f48414c3b62d7e7e58bb0ef22c0
jame <jameislucky@gmail.com>
2024-10-13 14:46:55 +0200

    Neue Funktionen

--- main.cpp
+++ main.cpp
@@ -3,6 +3,21 @@
 #include "Spieler.h"
 #include <vector>
 #include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+//#define DEBUG
+//#define ZUFALL_ANORDNUNG
+
+/*
+ * Wird der Kommentar vor ZUFALL_ANORDNUNG entfernt, so erscheint das Spielfeld in einem zufälligen Raster, d.h.
+ *  alle Punkte sind mit einer 50% Wahrscheinlichkeit entweder da oder nicht da. Dies verkürzt das Spiel und fügt
+ *  eine gewisse "Schwierigkeit" hinzu.
+ *
+ *  DEBUG fügt einen Debug-Modus hinzu, der beim Drücken der Taste "D" nach Beginn des Spiels sofort alle Punkte zählt
+ *  und zum Endbildschirm springt.
+ *  TO-DO: DEBUG addiert sich immer auf 256 Punkte.
+ */
 
 // sehr interessante Funktion
 float berechneAbstand(sf::Vector2f p1, sf::Vector2f p2) {
@@ -13,16 +28,25 @@ float berechneAbstand(sf::Vector2f p1, sf::Vector2f p2) {
 
 int main()
 {
-    sf::RenderWindow fenster(sf::VideoMode(512, 512), "Pacperson Prototype", sf::Style::Close);
-    fenster.setFramerateLimit(144);
+    sf::RenderWindow fenster(sf::VideoMode(512, 512), "Pacperson Alpha 0.3", sf::Style::Close);
+    sf::VideoMode desktopMode = sf::VideoMode::getDesktopMode();
+    fenster.setPosition(sf::Vector2i((desktopMode.width - 512) / 2, (desktopMode.height - 512) / 2));
+    fenster.setVerticalSyncEnabled(true);
+
+    //Zufallsgenerator
+    std::srand(static_cast<unsigned int>(std::time(0)));
+
 
     bool spielGestartet = false;
+    bool spielBeendet = false;
+    int gesammeltePunkte = 0;
 
     sf::Font schriftart;
-    if (!schriftart.loadFromFile("../assets/fonts/HomeVideo-BLG6G.ttf")){
+    if (!schriftart.loadFromFile("assets/fonts/HomeVideo-BLG6G.ttf")){
         return -1; // Fehler beim Laden
     }
 
+    //Willkommensbildschirm Text
     sf::Text titelText("Pac Person", schriftart, 50);
     titelText.setFillColor(sf::Color::White);
     titelText.setPosition(110.f, 200.f);
@@ -31,6 +55,14 @@ int main()
     anweisungsText.setFillColor(sf::Color::White);
     anweisungsText.setPosition(130.f, 300.f);
 
+    //Endbildschirm Text
+    sf::Text endText("Du hast gewonnen!", schriftart, 35);
+    endText.setFillColor(sf::Color::White);
+    endText.setPosition(50.f, 200.f);
+
+    sf::Text punktzahlText("", schriftart, 20);
+    punktzahlText.setFillColor(sf::Color::White);
+    punktzahlText.setPosition(130.f, 300.f);
 
 
     const int anzahlPunkte = 16; //die Zahl darf nicht mehr geändert werden sonst gibt es große Probleme
@@ -50,27 +82,59 @@ int main()
     // Initialisiere die Punkte und füge sie dem Vektor hinzu
     for (int i = 0; i < anzahlPunkte; ++i) {
         for (int j = 0; j < anzahlPunkte; ++j) {
-            float x = j * abstand + punktRadius;  // Berechne die x-Position
-            float y = i * abstand + punktRadius;  // Berechne die y-Position
-            punkte.emplace_back(x, y, punktRadius, sf::Color::White);
+#ifdef ZUFALL_ANORDNUNG
+            //50/50 Wahrscheinlichkeit für das Erscheinen einer Kugel
+            if(std::rand() % 2 == 0) {
+#endif
+                float x = j * abstand + punktRadius;  // Berechne die x-Position
+                float y = i * abstand + punktRadius;  // Berechne die y-Position
+                punkte.emplace_back(x, y, punktRadius, sf::Color::White);
+#ifdef ZUFALL_ANORDNUNG
+            }
+#endif
         }
     }
 
     //Hauptflow des Programms
-    while(fenster.isOpen()){
+    while(fenster.isOpen()) {
 
         sf::Event event;
-        while(fenster.pollEvent(event)){
-            if (event.type == sf::Event::Closed)
+
+        while (fenster.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
                 fenster.close();
-            if(!spielGestartet && event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Space){
+            }
+            //Spiel startet, wenn die Leertaste gedrückt wird
+            if (!spielGestartet && event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Space) {
                 spielGestartet = true;
             }
-            if(spielGestartet && event.type == sf::Event::KeyPressed){
+            //Spieler wird während des Spiels gesteuert
+            if (spielGestartet && !spielBeendet && event.type == sf::Event::KeyPressed) {
                 spieler.setzeRichtung(event.key.code);
             }
+
+#ifdef DEBUG
+        // Debug-Modus: Zum Endbildschirm springen (Taste 'D')
+        if (spielGestartet && !spielBeendet && event.type == sf::Event::KeyPressed &&
+            event.key.code == sf::Keyboard::D) {
+            punkte.clear();  // Alle Punkte "aufessen"
+            gesammeltePunkte = anzahlPunkte * anzahlPunkte;  // Maximale Punktzahl setzen
+            spielBeendet = true;
+            punktzahlText.setString("DEBUG: " + std::to_string(gesammeltePunkte));
+            }
+#endif
         }
-        if (spielGestartet) {
+
+        // Spielende überprüfen
+        if (spielGestartet && punkte.empty() && !spielBeendet) {
+            spielBeendet = true;
+
+            // Punktzahltext aktualisieren
+            punktzahlText.setString("Gesammelte Punkte: " + std::to_string(gesammeltePunkte));
+
+        }
+
+        if (spielGestartet && !spielBeendet) {
             if (uhr.getElapsedTime().asSeconds() > geschwindigkeit) {
                 spieler.bewegeAutomatisch();
                 uhr.restart();
@@ -80,6 +144,7 @@ int main()
                 float abstand = berechneAbstand(spieler.getPosition(), it->getPosition());
                 if (abstand < spieler.getRadius() + it->getRadius()) {
                     it = punkte.erase(it);  // Punkt entfernen, wenn er berührt wird
+                    gesammeltePunkte++;
                 } else {
                     ++it;  // Zum nächsten Punkt übergehen
                 }
@@ -91,7 +156,12 @@ int main()
                 punkt.zeichne(fenster);
             }
             spieler.zeichne(fenster);
-        } else{
+        }else if(spielBeendet){
+            fenster.clear(sf::Color::Black);  // Hintergrund Schwarz
+            fenster.draw(endText);
+            fenster.draw(punktzahlText);
+        }
+        else{
             fenster.clear(sf::Color::Black);  // Hintergrund Schwarz
             fenster.draw(titelText);
             fenster.draw(anweisungsText);
